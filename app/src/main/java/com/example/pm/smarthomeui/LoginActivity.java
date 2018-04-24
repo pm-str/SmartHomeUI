8882e6a1e89c801ea1b98cca227e46cbdf57ccbb
Michael Pavlov <pavl.mikhail@ya.ru>
2018-04-24 19:45:09 +0300

    Add error handling

--- app/src/main/java/com/example/pm/smarthomeui/LoginActivity.java
+++ app/src/main/java/com/example/pm/smarthomeui/LoginActivity.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -47,28 +48,11 @@ import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
-/**
- * A login screen that offers login via email/password.
- */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
     private UserLoginTask mAuthTask = null;
 
-    // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
@@ -106,6 +90,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        SharedPreferences preferences = getSharedPreferences("myPrefs", MODE_PRIVATE);
+        String token = preferences.getString("token","");
+        if (token.length() > 1) {
+            Intent intent = new Intent(context, HomeActivity.class);
+            startActivity(intent);
+        }
     }
 
     private void populateAutoComplete() {
@@ -117,9 +108,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
         if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
             return true;
         }
@@ -138,9 +126,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         return false;
     }
 
-    /**
-     * Callback received when a permissions request has been completed.
-     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
@@ -151,12 +136,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
     }
 
-
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
     private void attemptLogin() {
         if (mAuthTask != null) {
             return;
@@ -192,18 +171,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
 
         if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
             focusView.requestFocus();
         } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
-
-//            Intent intent = new Intent(this, HomeActivity.class);
-//            startActivity(intent);
         }
     }
 
@@ -215,9 +187,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         return password.length() > 4;
     }
 
-    /**
-     * Shows the progress UI and hides the login form.
-     */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
     private void showProgress(final boolean show) {
         int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
@@ -295,10 +264,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         int IS_PRIMARY = 1;
     }
 
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
         private final String mEmail;
@@ -341,16 +306,24 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
                 //Reading the response
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String s = bufferedReader.readLine();
+                String data = bufferedReader.readLine();
                 bufferedReader.close();
 
                 //Returning the response message to onPostExecute method
-                s = s.substring(10, 49);
+                String token = data.substring(10, 49);
+                String username = data.substring(data.indexOf("email")+8, data.indexOf("}")-1);
+
+                SharedPreferences preferences = getSharedPreferences("myPrefs", MODE_PRIVATE);
+                preferences.edit().putString("token", token).apply();
+                preferences.edit().putString("username", username).apply();
+
+                if (token.length() > 0 && username.length() > 0) {
+                    return true;
+                }
             } catch (IOException e) {
                 Log.e("Error: ", e.getMessage(), e);
             }
-
-            return true;
+            return false;
         }
 
         @Override
--- app/src/main/java/com/example/pm/smarthomeui/SettingsActivity.java
+++ app/src/main/java/com/example/pm/smarthomeui/SettingsActivity.java
@@ -2,13 +2,16 @@ package com.example.pm.smarthomeui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.borax12.materialdaterangepicker.time.RadialPickerLayout;
 import com.borax12.materialdaterangepicker.time.TimePickerDialog;
@@ -76,6 +79,25 @@ public class SettingsActivity extends AppCompatActivity {
                         false
                 );
                 tpd.show(getFragmentManager(), "Timepickerdialog");
+            }
+        });
+
+        SharedPreferences preferences = getSharedPreferences("myPrefs", MODE_PRIVATE);
+        String username = preferences.getString("username","");
+        if (username.length() > 18) {
+            username = username.substring(0, 15) + "...";
+        }
+        final TextView user_text_view = findViewById(R.id.user_text_id);
+        user_text_view.setText(username);
+
+        final Button buttonLogout = findViewById(R.id.logout_button_id);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences preferences = getSharedPreferences("myPrefs", MODE_PRIVATE);
+                preferences.edit().putString("token", "").apply();
+                Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
 
